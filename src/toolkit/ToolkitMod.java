bda9947b2bf95b2e366a71fe13f0c2aa42d11da5
KhanhLamVirual <136421486+KhanhLamVirual@users.noreply.github.com>
2023-08-13 20:18:28 +0700

    Update ToolkitMod.java

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -1,14 +1,53 @@
 package toolkit;
 
-import arc.util.*;
-import mindustry.mod.*;
-import toolkit.core.*;
+import arc.Events;
+import arc.util.Log;
+import mindustry.content.TechTree;
+import mindustry.ctype.UnlockableContent;
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
 
-public class ToolkitMod extends Mod {
+public class TookitMod extends Mod {
+    @SuppressWarnings("unused")
+    public UnlockTechTreeMod() {
+        Log.info("[UnlockTechTreeMod] Initializing...");
 
-    public ToolkitMod() {
-        Log.info("Loaded ToolkitMod constructor.");
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            BaseDialog dialog = new BaseDialog("Confirmation");
+            dialog.cont.add("Please confirm that you want to unlock all research on the tech-tree").row();
+            dialog.cont.button("Confirm", () -> {
+                dialog.hide();
 
-        TRenderer.init();
+                Log.info("[UnlockTechTreeMod] Unlocking tech-trees...");
+
+                for (TechTree.TechNode node : TechTree.all) {
+                    UnlockableContent content = node.content;
+                    if (content.locked()) {
+                        Log.info("[UnlockTechTreeMod] Unlocking content " + (content.name).replace("content", ""));
+                        content.unlock();
+                    }
+                }
+
+                Log.info("[UnlockTechTreeMod] Successfully unlocked tech-trees");
+
+                BaseDialog dialog2 = new BaseDialog("Success");
+                dialog2.cont.add("Successfully unlocked tech-trees").row();
+                dialog2.cont.add("Make sure to disable the mod to avoid seeing this dialogue again").row();
+                dialog2.cont.button("Ok", dialog2::hide).size(150F, 50F);
+                dialog2.show();
+            }).size(150F, 50F).row();
+
+            dialog.cont.button("Cancel", () -> {
+                dialog.hide();
+
+                BaseDialog dialog2 = new BaseDialog("Cancelled");
+                dialog2.cont.add("Cancelled unlocking tech-trees").row();
+                dialog2.cont.add("Disable the mod if you do not want to see this dialogue again").row();
+                dialog2.cont.button("Ok", dialog2::hide).size(150F, 50F);
+                dialog2.show();
+            }).size(150F, 50F);
+            dialog.show();
+        });
     }
 }
