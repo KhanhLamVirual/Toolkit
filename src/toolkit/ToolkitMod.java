cde3ef9db2aae38aebfc619f1d5362fec7bf39cb
KhanhLamVirual <136421486+KhanhLamVirual@users.noreply.github.com>
2023-08-16 09:27:08 +0700

    Update ToolkitMod.java

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -8,6 +8,7 @@ import mindustry.mod.Mod;
 import mindustry.type.Item;
 import mindustry.type.Liquid;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.Turret;
 
 public class ToolkitMod extends Mod {
     public ToolkitMod() {
@@ -17,17 +18,20 @@ public class ToolkitMod extends Mod {
             }
             Groups.build.each((build) -> {
                 Block block = build.block;
+                if (block instanceof Turret) {
+                    return;
+                }
                 for (Item item : Vars.content.items()) {
                     if (block.consumesItem(item)) {
-                        if (build.items.get(item) < block.itemCapacity+1000) {
-                            build.items.set(item, block.itemCapacity+2000);
+                        if (build.items.get(item) < block.itemCapacity) {
+                            build.items.set(item, block.itemCapacity);
                         }
                     }
                 }
                 for (Liquid liquid : Vars.content.liquids()) {
                     if (block.consumesLiquid(liquid)) {
-                        if (build.liquids().get(liquid) < block.liquidCapacity + 1000) {
-                            build.liquids.set(liquid, block.liquidCapacity +2000);
+                        if (build.liquids().get(liquid) < block.liquidCapacity) {
+                            build.liquids.set(liquid, block.liquidCapacity);
                         }
                     }
                 }
