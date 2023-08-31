d28c6ba4579c172c5471128decf4c927bf395f45
KhanhLamVirual <136421486+KhanhLamVirual@users.noreply.github.com>
2023-08-31 18:40:19 +0700

    Update ToolkitMod.java

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -18,24 +18,7 @@ public class ToolkitMod extends Mod {
             if (Vars.state.getState() != State.playing) {
                 return;
             }
-            Groups.build.each((build) -> {
-                Block block = build.block;
-                
-                for (Item item : Vars.content.items()) {
-                    if (block.consumesItem(item)) {
-                        if (build.items.get(item) < block.itemCapacity) {
-                            build.items.set(item, block.itemCapacity);
-                        }
-                    }
-                }
-                for (Liquid liquid : Vars.content.liquids()) {
-                    if (block.consumesLiquid(liquid)) {
-                        if (build.liquids().get(liquid) < block.liquidCapacity) {
-                            build.liquids.set(liquid, block.liquidCapacity);
-                        }
-                    }
-                }
-            });
+            
         }, 0.0f, 0.1f);
     }
 }
