9ffe1939eab123d6e456fc15c8bdfd4731ec684f
KhanhLamVirual <136421486+KhanhLamVirual@users.noreply.github.com>
2023-08-31 11:47:43 +0000

    Um

--- src/toolkit/ToolkitMod.java
+++ src/toolkit/ToolkitMod.java
@@ -3,6 +3,7 @@ package toolkit;
 import arc.util.Timer;
 import mindustry.Vars;
 import mindustry.core.GameState.State;
+import mindustry.game.Team;
 import mindustry.gen.Groups;
 import mindustry.mod.Mod;
 import mindustry.type.Item;
@@ -17,6 +18,12 @@ public class ToolkitMod extends Mod {
             Vars.state.rules.reactorExplosions = false;
             if (Vars.state.getState() != State.playing) {
                 return;
+            }
+
+            if (Team.sharded.core() != null) {
+                Vars.content.items().each((i) -> {
+                    Team.sharded.core().items().set(i, 21000);
+                });
             }
             
         }, 0.0f, 0.1f);
